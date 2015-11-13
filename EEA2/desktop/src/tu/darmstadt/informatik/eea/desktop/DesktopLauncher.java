51102bcfc8d6af18e30f9db710e36bf0585e4870
Tim B <tim93@online.de>
2015-11-13 15:18:55 +0100

    tmpstand

--- EEA2/desktop/src/tu/darmstadt/informatik/eea/desktop/DesktopLauncher.java
+++ EEA2/desktop/src/tu/darmstadt/informatik/eea/desktop/DesktopLauncher.java
@@ -3,11 +3,12 @@ package tu.darmstadt.informatik.eea.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import de.tu.darmstadt.informatik.eea.EEAGame;
+import de.tu.darmstadt.informatik.sampleGame.LaunchGame;
+
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new EEAGame(), config);
+		new LwjglApplication(new LaunchGame(), config);
 	}
 }
