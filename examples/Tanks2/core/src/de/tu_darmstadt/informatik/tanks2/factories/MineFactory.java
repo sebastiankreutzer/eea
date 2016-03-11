8475e59dbe2c7f0b1847e23a7f0d4d593dd9684b
johann <johann.reinhard@stud.tu-darmstadt.de>
2016-03-11 18:27:12 +0100

    Refactor and Comment for MineFactory

--- examples/Tanks2/core/src/de/tu_darmstadt/informatik/tanks2/factories/MineFactory.java
+++ examples/Tanks2/core/src/de/tu_darmstadt/informatik/tanks2/factories/MineFactory.java
@@ -2,7 +2,6 @@ package de.tu_darmstadt.informatik.tanks2.factories;
 
 import de.tu_darmstadt.informatik.eea.IResourcesManager;
 import de.tu_darmstadt.informatik.eea.action.AddComponentsAction;
-import de.tu_darmstadt.informatik.eea.action.DestroyEntityAction;
 import de.tu_darmstadt.informatik.eea.action.RemoveEventAction;
 import de.tu_darmstadt.informatik.eea.entity.Entity;
 import de.tu_darmstadt.informatik.eea.entity.ImageRenderComponent;
@@ -10,18 +9,40 @@ import de.tu_darmstadt.informatik.eea.event.CollisionEvent;
 import de.tu_darmstadt.informatik.eea.event.EEAEvent;
 import de.tu_darmstadt.informatik.eea.event.TimeEvent;
 import de.tu_darmstadt.informatik.tanks2.actions.HitAction;
-import de.tu_darmstadt.informatik.tanks2.entities.Mine;
 
+/**
+ * Eine MineFactory zum erzeugen von Minen.
+ * @author jr
+ *
+ */
 public class MineFactory {
 
 	private final boolean debug;
 	private IResourcesManager resourcesManager;
 	private ExplosionFactory explosionFactory;
-
+	
+	/**
+	 * Erzeugt eine neue MineFactory
+	 * 
+	 * @param resourcesManager
+	 *            Der ResourcesManager
+	 * @param explosionFactory
+	 *            Die ExplosionFactory
+	 */
 	public MineFactory(IResourcesManager resourcesManager, ExplosionFactory explosionFactory) {
 		this(resourcesManager, explosionFactory, false);
 	}
 
+	/**
+	 * Erzeugt eine neue MineFactory
+	 * 
+	 * @param resourcesManager
+	 *            Der ResourcesManager
+	 * @param explosionFactory
+	 *            Die ExplosionFactory
+	 * @param debug
+	 *            Der Zustand des Debugmodus
+	 */
 	public MineFactory(IResourcesManager resourcesManager, ExplosionFactory explosionFactory, boolean debug) {
 		this.resourcesManager = resourcesManager;
 		this.explosionFactory = explosionFactory;
@@ -29,25 +50,24 @@ public class MineFactory {
 	}
 
 	public Entity createEntity(float x, float y, float scale, int strength) {
-		Entity mine = new Mine("Mine" + Math.random(), strength);
+		// Erzeuge einen neue Entity und setze Skalierung und Position
+		Entity mine = new Entity("Mine" + Math.random());
 		mine.setScale(scale);
 		mine.setPosition(x, y);
 
+		// Fuege die RenderComponent hinzu
 		mine.addComponent(new ImageRenderComponent("mine.png", resourcesManager));
 
-		// If something collides with the mine, deal damage and destroy the
-		// mine.
+		// Erstelle ein CollisionEvent mit einer HitAction
 		EEAEvent collisionEvent = new CollisionEvent();
 		collisionEvent.addAction(new HitAction(strength, explosionFactory));
-		collisionEvent.addAction(new DestroyEntityAction());
 
-		// The mine should be armed with a delay.
+		// Die Mine soll erst nach einiger Zeit scharf gemacht werden
 		TimeEvent timeEvent = new TimeEvent(2.5f, false);
 		timeEvent.addAction(new AddComponentsAction(mine, collisionEvent));
 		timeEvent.addAction(new RemoveEventAction(timeEvent));
 
 		mine.addComponent(timeEvent);
-
 		return mine;
 	}
 
