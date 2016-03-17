0eb21b0cbd2a9af106377ea89c4c982c9e8e33ea
johann <johann.reinhard@stud.tu-darmstadt.de>
2016-03-18 00:40:05 +0100

    Comments Tanks2 Exceptions.

--- examples/Tanks2/core/src/de/tu_darmstadt/informatik/tanks2/exceptions/SemanticException.java
+++ examples/Tanks2/core/src/de/tu_darmstadt/informatik/tanks2/exceptions/SemanticException.java
@@ -1,10 +1,12 @@
 package de.tu_darmstadt.informatik.tanks2.exceptions;
 
+/**
+ * Eine Exception die bei einem semantischem Fehler beim Parsen geworfen wird.
+ * @author jr
+ *
+ */
 public class SemanticException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public SemanticException() {
--- examples/Tanks2/core/src/de/tu_darmstadt/informatik/tanks2/exceptions/SyntaxException.java
+++ examples/Tanks2/core/src/de/tu_darmstadt/informatik/tanks2/exceptions/SyntaxException.java
@@ -1,10 +1,12 @@
 package de.tu_darmstadt.informatik.tanks2.exceptions;
 
+/**
+ * Eine Exception die bei einem syntaktischen Fehler beim Parsen geworfen wird.
+ * @author jr
+ *
+ */
 public class SyntaxException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public SyntaxException() {
