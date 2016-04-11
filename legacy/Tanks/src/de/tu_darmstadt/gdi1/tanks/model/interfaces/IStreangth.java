76e1b5dcaaae3b04c02d46b045c9e6dbe2c88faa
Sebastian Kreutzer <SebastianKreutzer@gmx.net>
2016-04-11 17:16:52 +0200

    Delete old test code

--- legacy/Tanks/src/de/tu_darmstadt/gdi1/tanks/model/interfaces/IStreangth.java
+++
@@ -1,9 +0,0 @@
-package de.tu_darmstadt.gdi1.tanks.model.interfaces;
-
-public interface IStreangth {
-	
-	public void changeStreangth(int value);
-	public int getStreangth();
-	public void setStreangth(int streangth);
-
-}
