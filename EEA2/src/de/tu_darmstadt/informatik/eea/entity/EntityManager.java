8248e180e335c5d906adb137bdf797ba66ff3ca9
TimBo93 <tim93@online.de>
2016-04-14 11:05:41 +0200

    comments for entitymanager

--- EEA2/src/de/tu_darmstadt/informatik/eea/entity/EntityManager.java
+++ EEA2/src/de/tu_darmstadt/informatik/eea/entity/EntityManager.java
@@ -33,31 +33,53 @@ public class EntityManager {
 		entities = new ArrayList<Entity>();
 	}
 
+	/**
+	 * Adds given Entity to this {@link Stage} and sets this instance as EntityManager.
+	 * @param e
+	 */
 	public void addEntity(Entity e) {
 		stage.addActor(e);
 		e.setManager(this);
 	}
 	
+	/**
+	 * calls addEntity foreach Entity in given list
+	 * @param entities
+	 */
 	public void addEntities(List<Entity> entities) {
 		for (Entity entity : entities) {
 			addEntity(entity);
 		}
 	}
 
-	public Entity getEntity(String name) {
+	/**
+	 * Finds an Entity by its id. Returns the first added entity with matching id.
+	 * @param id
+	 * @return
+	 */
+	public Entity getEntity(String id) {
 		Iterator<Entity> it = entities.iterator();
 		while (it.hasNext()) {
 			Entity e = it.next();
-			if (e.getID().equals(name))
+			if (e.getID().equals(id))
 				return e;
 		}
 		return null;
 	}
 
+	/**
+	 * returns all entities.
+	 * @return
+	 */
 	public List<Entity> getAllEntities() {
 		return entities;
 	}
 
+	/**
+	 * checks wehter an entity exists with matching id-prefix
+	 * @param prefix
+	 * @return
+	 */
 	public boolean hasEntity(String prefix) {
 		Iterator<Entity> it = entities.iterator();
 		while (it.hasNext()) {
@@ -68,11 +90,20 @@ public class EntityManager {
 		return false;
 	}
 
+	/**
+	 * removes one Entity and unsets its EntityManager
+	 * @param e
+	 */
 	public void removeEntity(Entity e) {
 		entities.remove(e);
 		e.setManager(null);
 	}
 
+	/**
+	 * Returns one colliding Entity with given Entity. Returns null if there is no colliding entity.
+	 * @param e
+	 * @return
+	 */
 	public Entity collides(Entity e) {
 		Iterator<Entity> it = entities.iterator();
 		while (it.hasNext()) {
@@ -83,6 +114,11 @@ public class EntityManager {
 		return null;
 	}
 
+	/**
+	 * Returns a list of all colliding Entities with given Entity. Returns an empty list, if there is no colliding one.
+	 * @param e
+	 * @return
+	 */
 	public List<Entity> getAllCollisions(Entity e) {
 		List<Entity> collisions = new ArrayList<Entity>();
 		Iterator<Entity> it = entities.iterator();
@@ -95,19 +131,29 @@ public class EntityManager {
 	}
 
 
-
+	/**
+	 * updates all entities.
+	 * @param delta
+	 */
 	public void update(float delta) {
 		stage.act(delta);
 	}
 
+	/**
+	 * renders all entities.
+	 */
 	public void renderEntities() {
 		stage.draw();
 	}
 
+	/**
+	 * resets this EntityManager. All entities will be removed. 
+	 */
 	public void reset() {
 		stage.clear();
 		while(entities.size() > 0) {
 			entities.get(0).remove();
+			
 		}
 //		while(iterator.hasNext()) {
 //			Entity e = iterator.next();
@@ -121,6 +167,10 @@ public class EntityManager {
 		stage.dispose();
 	}
 
+	/**
+	 * Enable or disable debugging.
+	 * @param b
+	 */
 	public void setDebug(boolean b) {
 		stage.setDebugAll(b);
 	}
