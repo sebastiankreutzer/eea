1fa14149179f474ee15e9f847a86edda85c59e55
Johann <johann.reinhard@stud.tu-darmstadt.de>
2016-04-15 12:07:29 +0200

    Comment for EEAGameState class

--- EEA2/src/de/tu_darmstadt/informatik/eea/EEAGameState.java
+++ EEA2/src/de/tu_darmstadt/informatik/eea/EEAGameState.java
@@ -8,19 +8,29 @@ import com.badlogic.gdx.graphics.GL20;
 
 import de.tu_darmstadt.informatik.eea.entity.EntityManager;
 
+/**
+ * This is a template for a game state in the EEA framework. Each GameState has
+ * its own list of entities, managed by its EntityManager. To switch to a state,
+ * use {@link de.tu_darmstadt.informatik.eea.EEAGame#setScreen(Screen)}, see also {@link de.tu_darmstadt.informatik.eea.action.ChangeStateAction}.
+ * 
+ * @author Johann Reinhard
+ *
+ */
 public abstract class EEAGameState implements Screen {
 
 	protected final EEAGame game;
 	protected final EntityManager em;
-	protected final InputMultiplexer im;
-	private Color backgroundColor = new Color(0,0,0,0);
+	private final InputMultiplexer im;
+	private Color backgroundColor = new Color(0, 0, 0, 0);
 
 	private boolean paused = false;
 	private boolean initialized = false;
 
 	/**
 	 * Creates a new game state.
-	 * @param game The current game object.
+	 * 
+	 * @param game
+	 *            The current game object.
 	 */
 	public EEAGameState(EEAGame game) {
 		this.game = game;
@@ -31,14 +41,17 @@ public abstract class EEAGameState implements Screen {
 	}
 
 	/**
-	 * Updates the game.
-	 * Any changes in the current game state are performed here.
-	 * @param delta Elapsed time in seconds.
+	 * Updates the game. Any changes in the current game state are performed
+	 * here.
+	 * 
+	 * @param delta
+	 *            Elapsed time in seconds.
 	 */
 	protected abstract void update(float delta);
 
 	/**
-	 * Initializes the game state.
+	 * Initializes the game state. Is called every time this game state is set
+	 * as the active game state AND not initialized.
 	 */
 	protected abstract void init();
 
@@ -47,17 +60,21 @@ public abstract class EEAGameState implements Screen {
 		Gdx.input.setInputProcessor(im);
 		updateBackgroundColor();
 		if (!initialized) {
-			initialized = true;
 			init();
+			initialized = true;
 		}
 	}
 
 	/**
-	 * Sets the background color that is used to clear the screen.
-	 * Color components should be in range 0 - 255.
-	 * @param r Red component
-	 * @param g Green component
-	 * @param b Blue component
+	 * Sets the background color that is used to clear the screen. Color
+	 * components should be in range 0 - 255.
+	 * 
+	 * @param r
+	 *            Red component
+	 * @param g
+	 *            Green component
+	 * @param b
+	 *            Blue component
 	 */
 	public void setBackgroundColor(int r, int g, int b) {
 		this.backgroundColor = new Color(r / 255f, g / 255f, b / 255f, 1.0f);
@@ -73,7 +90,9 @@ public abstract class EEAGameState implements Screen {
 
 	/**
 	 * Updates and renders the game.
-	 * @param delta Elapsed time in seconds.
+	 * 
+	 * @param delta
+	 *            Elapsed time in seconds.
 	 */
 	@Override
 	public void render(float delta) {
@@ -91,7 +110,8 @@ public abstract class EEAGameState implements Screen {
 	}
 
 	/**
-	 * Resets the game state.
+	 * Resets this game state. It will be initialized again the next time it is
+	 * set as the active screen.
 	 */
 	public void reset() {
 		em.reset();
